d41134ea995abc9789ce9b42325f71ebcc77dd99
Dave Carter <dcarter@velma.com>
2015-01-26 14:12:54 -0700

    Initial Files added

---
+++ mainController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by dhcarter on 1/26/2015.
+ */
+
+myApp.controller('mainController', function ($scope) {
+
+
+    $scope.hi = function (area) {
+
+        sayhi(area);
+
+    }
+
+
+});
